eca376efe2d4c39b411c5025f7c5e62eb2d72206
830Utpal <lakshman2016yadav@gmail.com>
2025-04-19 02:09:51 +0530

    card

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './components/theme-provider';
 import Home from './pages/Home/Home';
 import Dashboard from './pages/Dashboard/dashboard';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 function App() {
   return (
