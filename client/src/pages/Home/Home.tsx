ba45e17167ca8dfdcae907763c9a530665ce5a85
RITIK-CHAUDHRY <ritikchaudhry1305@gmail.com>
2025-04-19 11:21:36 +0530

    add what we offer?

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,11 +1,15 @@
-import Hero from "@/components/shared/Hero";
-import Navbar from "@/components/shared/Navbar";
+import Hero from '@/components/shared/Hero';
+import Navbar from '@/components/shared/Navbar';
+import WhatWeOffer from '@/components/shared/WhatWeOffer';
+import SuggestionForm from '@/components/shared/SuggestionForm';
 
 function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <WhatWeOffer />
+      <SuggestionForm />
     </div>
   );
 }
